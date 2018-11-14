d53eb35213eac2e8569390cd1edbf2ba96e9eef8
EDubaj <ewadubaj96@gmail.com>
2018-11-14 20:33:18 +0100

    Add frontend

---
+++ etl-frontend/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Typography from "@material-ui/core/Typography/Typography";
+
+class App extends Component {
+  render() {
+    return (
+        <Typography component="h2" variant="h1" gutterBottom>
+              Ceneo ETL - frontend
+        </Typography>
+    );
+  }
+}
+
+export default App;
