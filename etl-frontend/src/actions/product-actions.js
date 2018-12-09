734f2a53dd9eacdf4b11bec263a9cfc033245ab4
EDubaj <ewadubaj96@gmail.com>
2018-12-09 13:08:14 +0100

    Implement transform/load steps

--- etl-frontend/src/actions/product-actions.js
+++ etl-frontend/src/actions/product-actions.js
@@ -1,6 +1,18 @@
 import axios from "axios";
-import {API_ERROR, API_SUCCESS, FETCH_PRODUCTS_DETAILS_ERROR, FETCH_PRODUCTS_DETAILS_SUCCESS} from "./types";
-import {EXTRACT_ENDPOINT, PRODUCT_ENDPOINT, PRODUCTS_ENDPOINT} from "../config/data-properties";
+import {
+    API_ERROR,
+    API_SUCCESS,
+    FETCH_PRODUCTS_DETAILS_ERROR,
+    FETCH_PRODUCTS_DETAILS_SUCCESS,
+    TRANSFORM_PRODUCT_SUCCESS
+} from "./types";
+import {
+    EXTRACT_ENDPOINT,
+    LOAD_ENDPOINT,
+    PRODUCT_ENDPOINT,
+    PRODUCTS_ENDPOINT,
+    TRANSFORM_ENDPOINT
+} from "../config/data-properties";
 
 export function fetchProducts() {
     return dispatch =>
@@ -49,6 +61,41 @@ export function extractProduct(productId, actionId) {
                 actionId: actionId,
                 payload: {
                     message: 'Unable to extract product. Make sure that provided ID is correct and item is not already in our database'
+                }
+            }))
+}
+
+export function transformProduct(productId, actionId) {
+    return dispatch =>
+        axios.get(`${TRANSFORM_ENDPOINT}`)
+            .then(response => dispatch({
+                type: TRANSFORM_PRODUCT_SUCCESS,
+                payload: response.data
+            }))
+            .catch(error => dispatch({
+                type: API_ERROR,
+                actionId: actionId,
+                payload: {
+                    message: 'Unable to transform product'
+                }
+            }))
+}
+
+export function loadProduct(actionId) {
+    return dispatch =>
+        axios.get(`${LOAD_ENDPOINT}`)
+            .then(response => dispatch({
+                type: API_SUCCESS,
+                actionId: actionId,
+                payload: {
+                    message: 'Successfully loaded product'
+                }
+            }))
+            .catch(error => dispatch({
+                type: API_ERROR,
+                actionId: actionId,
+                payload: {
+                    message: 'Unable to load product'
                 }
             }))
 }
